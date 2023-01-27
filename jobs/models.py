e71b6719afc6e82b1ce44013b5fd74664f43cf69
nferrari3444 <nferrari3444@gmail.com>
2023-01-27 20:40:27 -0300

    First Commit

---
+++ jobs/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Job(models.Model):
+    # image = models.ImageField(upload_to='images/')
+    # summary = models.CharField(max_length=200)
+
+    image = models.ImageField(upload_to='images/')
+    # summary 
+    summary = models.CharField(max_length=200)
+    title = models.CharField(max_length=20, default="recipe-app")
+    link = models.CharField(max_length=50, default='link')
+    # If anyone wants to know what's the description of this particular object is , can see the summary.
+    def __str__(self):
+        return self.summary
