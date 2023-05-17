1c2d5c15e28407d9032baf9fe78caefec1096511
nferrari3444 <nferrari3444@gmail.com>
2023-05-16 22:17:37 -0300

    change layout

--- jobs/models.py
+++ jobs/models.py
@@ -10,6 +10,7 @@ class Job(models.Model):
     summary = models.CharField(max_length=200)
     title = models.CharField(max_length=20, default="recipe-app")
     link = models.CharField(max_length=50, default='link')
+    description = models.CharField(max_length= 600, default="description")
     # If anyone wants to know what's the description of this particular object is , can see the summary.
     def __str__(self):
         return self.summary
