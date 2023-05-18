33f4e6b9c1d0de3a76fa551848b7b9504c2493f3
nferrari3444 <nferrari3444@gmail.com>
2023-05-17 23:58:15 -0300

    add base file for deploying

--- newportfolio/settings/prod.py
+++ newportfolio/settings/prod.py
@@ -6,7 +6,7 @@ env= os.environ.copy()
 
 # SECRET_KEY=env['SECRET_KEY']
 
-try:
-    from newportfolio.settings.local import *
-except:
-    pass
+# try:
+#     from newportfolio.settings.local import *
+# except:
+#     pass
